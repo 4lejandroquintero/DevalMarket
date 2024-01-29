874b181649cf37df8beb954c77059a38b93df377
4lejandroquintero <alejoq96@gmail.com>
2024-01-29 18:20:08 -0500

    estructura del proyecto

---
+++ src/main/java/com/deval/market/DevalMarketApplication.java
@@ -0,0 +1,13 @@
+package com.deval.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevalMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevalMarketApplication.class, args);
+	}
+
+}
